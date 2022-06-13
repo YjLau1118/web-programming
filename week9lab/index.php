478b034b161f809d4e3372af12f4aa1463d4e0a0
YjLau1118 <93077477+YjLau1118@users.noreply.github.com>
2022-06-13 12:25:23 +0800

    Add files via upload

---
+++ week9lab/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Week 9 Lab Deliverables</title>
+</head>
+<body>
+<table width="100%" border="0" cellpadding="20">
+  <tr>
+    <td colspan="2" bgcolor="DimGray">
+      <h1 align="center"><font color="ffffff" face="verdana">Home Page</font></h1>
+    </td>
+  </tr>
+  <tr>
+    <td align="center">
+      <a href="content.php">
+      <button style="height:100px; width:200px;">Content</button></a> 
+    </td>
+  </tr>
+  <tr>
+    <td align="center">
+      <a href="bio_form.php">
+      <button style="height:100px; width:200px;">Bio Form</button></a>
+    </td>
+  </tr>
+
+    <td colspan="2" bgcolor="Khaki">
+      <center>
+      TTTP2543 - Web Programming
+      </center>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
