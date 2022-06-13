29d803a8d056c4ab47fdb8c3d48b0218a15dd5d7
YjLau1118 <93077477+YjLau1118@users.noreply.github.com>
2022-06-13 12:19:42 +0800

    Add files via upload

---
+++ week4lab/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Week 4 Lab Deliverables</title>
+</head>
+<body>
+<table width="100%" border="0" cellpadding="20">
+  <tr>
+    <td colspan="2" bgcolor="DimGray">
+      <h1 align="center"><font color="ffffff" face="verdana">Home Page</font></h1>
+    </td>
+  </tr>
+  <tr>
+    <td align="center">
+      <a href="bio_form.php">
+      <button style="height:100px; width:200px;">Bio Form</button></a> 
+    </td>
+  </tr>
+  <tr>
+    <td align="center">
+      <a href="edit_bio.php">
+      <button style="height:100px; width:200px;">Edit Bio</button></a>
+    </td>
+  </tr>
+  <tr>
+    <td align="center">
+      <a href="lecturers_list.php">
+      <button style="height:100px; width:200px;">Lectures List</button></a>
+    </td>
+  </tr>
+  <tr>
+    <td colspan="2" bgcolor="Khaki">
+      <center>
+      TTTP2543 - Web Programming
+      </center>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
